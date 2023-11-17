66f07a61a17beb87f0832ccd7354f25fea226b57
Abdullah Seyidleroglu <seyidleroglu@gmail.com>
2023-11-17 17:21:26 +0300

    .

---
+++ APPUI/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="wrapper">
+  <!-- <Preloader/> -->
+  <Navbar/>
+  <Sidebar/>
+    <div class="content-wrapper">
+    <RouterView />
+    </div>
+     <FooterX/>
+       <!-- Control Sidebar -->
+      <aside class="control-sidebar control-sidebar-dark">
+        <!-- Control sidebar content goes here -->
+      </aside>
+      <!-- /.control-sidebar -->
+</div> 
+</template>
+
+<script>
+// import Preloader from '@/views/Layout/Preloader.vue';
+import Navbar from '@/views/Layout/Navbar.vue';
+import Sidebar from '@/views/Layout/Sidebar.vue';
+import FooterX from '@/views/Layout/footer.vue';
+export default {
+    components: {
+        Navbar, Sidebar, FooterX
+    }
+}
+
+</script>
