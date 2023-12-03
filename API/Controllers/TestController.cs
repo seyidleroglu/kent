4351a516b2fdc50780b33d7d7e0d97b2d9a4a873
Abdullah Seyidleroglu <seyidleroglu@gmail.com>
2023-12-04 00:13:05 +0300

    api kurulum ve mysql bağlantısı yapıldı

---
+++ API/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+        public TestController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet]
+        public Task<IActionResult> UsersList()
+        {
+            var list = _appDbContext.Users.ToList();
+            return Task.FromResult<IActionResult>(Ok(list));
+        }
+    }
+}
---
+++ API/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        
+    }
+}
